aee0299acf2d494b795b446836598f42c98f1995
zhekeani <zhekeani@gmail.com>
2025-03-07 09:13:17 +0700

    chore: add topics fetch function

---
+++ src/lib/component-fetches/topic/fetchTopicsServer.ts
@@ -0,0 +1,25 @@
+import { getSupabaseCookiesUtilClient } from "@/supabase-utils/cookiesUtilClient";
+
+export const fetchHomeTopicsOnServer = async () => {
+  try {
+    const supabase = await getSupabaseCookiesUtilClient();
+    if (!supabase) {
+      throw new Error("Database client unavailable.");
+    }
+
+    const { data: topics, error } = await supabase
+      .from("topics")
+      .select("id, name")
+      .in("depth_level", [0, 1])
+      .limit(10);
+
+    if (error || !topics) {
+      throw new Error("Failed to fetch topics.");
+    }
+
+    return topics;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
